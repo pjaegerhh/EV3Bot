e560ffe983e3bf067042120d453d388f232cbf1f
Peter Jaeger <peter@jaeger.to>
2017-06-21 21:52:30 +0200

    v .06

--- messages/index.js
+++ messages/index.js
@@ -29,13 +29,13 @@ var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.micro
 const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
 
 console.log('%s listening to %s', LuisModelUrl, luisAppId);
-session.send('Moin %s', LuisModelUrl);
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 bot.recognizer(recognizer);
 bot.dialog('pjEV3_help', function (session) {
 
+    session.send('Moin %s', LuisModelUrl);
     session.endDialog('Hi ! Versuche doch mal folgende Kommandos : Fahre Vorwärts, Fahre Rückwärts')
 
 }).triggerAction({
